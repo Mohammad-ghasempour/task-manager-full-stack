db511177788ecd1e014722f189e02a21738dae9c
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-11-20 10:38:54 +0100

    using Template Engines to use javascript in the html files

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ import "dotenv/config";
 import express from "express";
 
 import getRoute from "./routes/get-routes.js";
-import postRoute from "./routes/post-routes.js"
+import postRoute from "./routes/post-routes.js";
 
 const app = express();
-
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
 
 app.use(getRoute);
 app.use(postRoute);
 app.listen(3000);
 
-
-export {__dirname as rootPath};
+export { __dirname as rootPath };
--- controllers/get-controller.js
+++ controllers/get-controller.js
@@ -1,12 +1,11 @@
 import Task from "../models/task.js";
-import { rootPath } from "../app.js";
 
 
 export default class GetController {
 
     static homeController(req,res){
         const tasks = Task.getAllTasks();
-        res.sendFile(rootPath + '/view/home.html');
+        res.render('home' , {tasks});
     }
 
 }
