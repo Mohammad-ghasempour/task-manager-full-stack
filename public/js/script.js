51021d1dc33da15f299c347ee1d1483e6433aab9
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-11-27 17:17:21 +0100

    toggle button changed with new RESTFILL rule

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,8 @@
 const list = document.querySelector(".list-group");
 const emptyTaskMessage = document.querySelector("h2");
-const addButton = document.getElementById('addButton');
-const input = document.getElementById('myInput');
-const checkbox = document.getElementById('my-checkbox');
-
-
+const addButton = document.getElementById("addButton");
+const input = document.getElementById("myInput");
+const checkbox = document.getElementById("my-checkbox");
 
 list.addEventListener("click", async (event) => {
    const target = event.target;
@@ -12,8 +10,17 @@ list.addEventListener("click", async (event) => {
 
    if (target.classList.contains("toggle-btn")) {
       try {
-         const response = await axios.post("/toggle-task", { id: id });
-         if (response.data === true) {
+         const title = target.parentElement.querySelector("label").textContent;
+         const completed =
+            target.parentElement.querySelector("span").lastElementChild
+               .innerHTML === "Completed"
+               ? false
+               : true;
+         const { data } = await axios.put("/tasks/" + id, {
+            title,
+            completed,
+         });
+         if (data.success) {
             target.parentElement.querySelector(
                "span"
             ).lastElementChild.innerHTML =
@@ -30,10 +37,10 @@ list.addEventListener("click", async (event) => {
                   ? "bg-success"
                   : "bg-secondary";
          } else {
-            alert(response.data);
+            alert(data.message);
          }
       } catch (err) {
-         console.log(err.response); //with err.response you can get more human readable error!
+         console.log(err.response.data.message); //with err.response you can get more human readable error!
       }
    } else if (target.classList.contains("edit-btn")) {
       const currentTitle =
@@ -77,9 +84,8 @@ list.addEventListener("click", async (event) => {
          } catch (err) {
             alert(err.response.data);
          }
-         if (list.innerHTML.trim() == ""){
-         emptyTaskMessage.classList.remove("d-hide");
-
+         if (list.innerHTML.trim() == "") {
+            emptyTaskMessage.classList.remove("d-hide");
          }
       }
    }
@@ -87,85 +93,79 @@ list.addEventListener("click", async (event) => {
 
 document.addEventListener("DOMContentLoaded", async () => {
    try {
-      const { data } = await axios.get("/get-all-tasks");
-      if (data instanceof Array) {
-         if (data.length) {
+      const { data } = await axios.get("/tasks");
+      if (data.success) {
+         if (data.body.length) {
             list.classList.remove("d-hide");
             let listItem = "";
-            for (let task of data) {
+            for (let task of data.body) {
                listItem += `
-            <li data-taskId='${task.id}'>
-    <span>
-       <label>${task.title}</label>
-       <span class= ${task.completed ? "bg-success" : "bg-secondary"} > ${
+                     <li data-taskId='${task.id}'>
+            <span>
+               <label>${task.title}</label>
+               <span class= ${task.completed ? "bg-success" : "bg-secondary"}>${
                   task.completed ? "Completed" : "In progress"
-               } </span>
-    </span>
-    <button class="btn-secondary toggle-btn">Toggle</button>
-    <button class="btn-primary edit-btn">Edit</button>
-    <button class="btn-danger delete-btn">Delete</button>
- </li>
- `;
+               }</span>
+            </span>
+            <button class="btn-secondary toggle-btn">Toggle</button>
+            <button class="btn-primary edit-btn">Edit</button>
+            <button class="btn-danger delete-btn">Delete</button>
+            </li>
+               `;
                list.innerHTML = listItem;
             }
          } else {
             emptyTaskMessage.classList.remove("d-hide");
          }
+      } else {
+         alert(data.message);
       }
    } catch (err) {
-      console.log(err.message);
+      console.log(err.response.data.message);
    }
 });
 
- addButton.addEventListener('click', addTask);
- input.addEventListener('keydown',(event)=>{
-    if (event.key === 'Enter'){
-      event.preventDefault()
+addButton.addEventListener("click", addTask);
+input.addEventListener("keydown", (event) => {
+   if (event.key === "Enter") {
+      event.preventDefault();
       addTask();
    }
- })
+});
 
- 
-async function  addTask(){
-
-     const title = input.value;
-     const completed = checkbox.checked;
-     if (title.length <3){
-      alert('Please enter at least 3 character as your task title.')
+async function addTask() {
+   const title = input.value;
+   const completed = checkbox.checked;
+   if (title.length < 3) {
+      alert("Please enter at least 3 character as your task title.");
       return;
-     }
-     
-     try {
-      const {data} = await axios.post("/addData", {title, completed});
-      if (data>0) {
-         emptyTaskMessage.classList.add('d-hide');
-         list.classList.remove("d-hide");
-
-      
-      const newItem = `
-         <li data-taskId='${data}'>
- <span>
-    <label>${title}</label>
-    <span class= ${completed ? "bg-success" : "bg-secondary"} > ${
-               completed ? "Completed" : "In progress"
-            } </span>
- </span>
- <button class="btn-secondary toggle-btn">Toggle</button>
- <button class="btn-primary edit-btn">Edit</button>
- <button class="btn-danger delete-btn">Delete</button>
-</li>
-`
-list.innerHTML += newItem
-input.value=''
-
-      } else {
-         alert(data);
-      }
-   } catch (err) {
-      alert(err.message);
    }
 
+   try {
+      const { data } = await axios.post("/tasks", { title, completed });
+      if (data.success) {
+         emptyTaskMessage.classList.add("d-hide");
+         list.classList.remove("d-hide");
+
+         const newItem = `
+                        <li data-taskId='${data.body.id}'>
+               <span>
+                  <label>${title}</label>
+                  <span class= ${completed ? "bg-success" : "bg-secondary"} > ${
+            completed ? "Completed" : "In progress"
+         } </span>
+               </span>
+               <button class="btn-secondary toggle-btn">Toggle</button>
+               <button class="btn-primary edit-btn">Edit</button>
+               <button class="btn-danger delete-btn">Delete</button>
+               </li>
+               `;
+         list.innerHTML += newItem;
+         input.value = "";
+      } else {
+         alert(data.message);
+      }
+   } catch (err) {
+      alert(err.response.data.message);
+   }
 }
-
-
- 
